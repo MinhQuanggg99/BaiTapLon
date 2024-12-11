4c44c96ced59e6f55911b3538b95203991b70bf9
unknown <MinhQuangg1199@gmail.com>
2024-12-11 11:53:54 +0700

    Fix cac loi con lai va hoan thien bai tap lon

--- BaiTapLon/obj/Debug/net9.0/BaiTapLon.AssemblyInfo.cs
+++ BaiTapLon/obj/Debug/net9.0/BaiTapLon.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BaiTapLon")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2cc48bf9a37038f82c11c08f915d51b7144d964e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf52584826047905ad2079c74d7aa9fc30a3e6df")]
 [assembly: System.Reflection.AssemblyProductAttribute("BaiTapLon")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BaiTapLon")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
