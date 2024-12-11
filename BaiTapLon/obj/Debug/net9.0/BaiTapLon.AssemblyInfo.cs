21fc56c133974984a738d9b62382d0d332cefa5a
unknown <MinhQuangg1199@gmail.com>
2024-12-11 12:04:31 +0700

    fix dieu huong

--- BaiTapLon/obj/Debug/net9.0/BaiTapLon.AssemblyInfo.cs
+++ BaiTapLon/obj/Debug/net9.0/BaiTapLon.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BaiTapLon")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf52584826047905ad2079c74d7aa9fc30a3e6df")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c44c96ced59e6f55911b3538b95203991b70bf9")]
 [assembly: System.Reflection.AssemblyProductAttribute("BaiTapLon")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BaiTapLon")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
